5e9b3d06391eb914eed13c51fb0aff41573ed754
unknown <ermolov1337@gmail.com>
2021-12-27 11:28:50 +0300

    add fetch

---
+++ src/containers/App/App.jsx
@@ -0,0 +1,12 @@
+import style from './App.module.css';
+
+import { getApiResource } from '../../utils/network'
+
+
+function App() {
+  return (
+    <h1 className={style.header}>Hello</h1>
+  );
+}
+
+export default App;
