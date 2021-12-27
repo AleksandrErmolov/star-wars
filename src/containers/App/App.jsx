547d4d9f96889790ed77f7de1eb4beaebfa0beee
unknown <ermolov1337@gmail.com>
2021-12-27 21:06:02 +0300

    add foto and css

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,12 +1,17 @@
+import PeoplePage from '../PeoplePage/PeoplePage';
 import style from './App.module.css';
 
-import { getApiResource } from '../../utils/network'
 
 
 function App() {
   return (
-    <h1 className={style.header}>Hello</h1>
-  );
+
+    <div>
+      <PeoplePage />
+    </div>
+
+
+    );
 }
 
 export default App;
