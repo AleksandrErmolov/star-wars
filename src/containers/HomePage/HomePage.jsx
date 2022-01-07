e1920aa01180db0726bab1d4fc76bb6991b15400
unknown <ermolov1337@gmail.com>
2022-01-07 23:28:24 +0300

    add new title

--- src/containers/HomePage/HomePage.jsx
+++ src/containers/HomePage/HomePage.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import ChooseSide from '../../components/HomePage/ChooseSide/ChooseSide';
+import React from "react";
+import ChooseSide from "../../components/HomePage/ChooseSide/ChooseSide";
 import styles from "./HomePage.module.css";
 
-
 export default function HomePage() {
-    return (
-        <div>
-            <h1 className='header__text'>HOME PAGE</h1>
-            <ChooseSide />
-        </div>
-    )
+  return (
+    <div>
+      <h1 className="header__text">Choose your side</h1>
+      <ChooseSide />
+    </div>
+  );
 }
